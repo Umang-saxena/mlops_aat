e5e3393b22f0338a7fcc1adbe3f2c5a1bdd6f971
Umang Saxena <hanusaxena68@gmail.com>
2025-05-23 23:20:35 +0530

    Github Actions configured

---
+++ predict_api.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+
+app = Flask(__name__)
+X, y = load_iris(return_X_y=True)
+model = LogisticRegression(max_iter=200).fit(X, y)
+
+PORT_ID = "5000"  # Change this manually per instance
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['features']
+    prediction = model.predict([data])
+    return jsonify({
+        'prediction': int(prediction[0]),
+        'served_by': f'Instance running on port {PORT_ID}'
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, port=int(PORT_ID))
